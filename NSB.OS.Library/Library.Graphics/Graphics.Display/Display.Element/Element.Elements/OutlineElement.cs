2b4d424b392ef09dac229e4dac405676fa1414cf
NebulaDev <nebuladev.contact@gmail.com>
2023-03-03 11:34:12 +1300

    Add `CenteredTextElement`

---
+++ NSB.OS.Library/Library.Graphics/Graphics.Display/Display.Element/Element.Elements/OutlineElement.cs
@@ -0,0 +1,56 @@
+using NSB.OS.Graphics.Mathematics;
+
+namespace NSB.OS.Graphics.DisplayNS;
+
+public class OutlineElement : Element {
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public RGB? BG { get; set; }
+    public RGB? FG { get; set; }
+
+    public char tl = '┌';
+    public char tr = '┐';
+    public char bl = '└';
+    public char br = '┘';
+    public char h = '─';
+    public char v = '│';
+
+    public OutlineElement(int x, int y, int width, int height, RGB? bg = null, RGB? fg = null) {
+        this.X = x;
+        this.Y = y;
+        Width = width;
+        Height = height;
+        BG = bg;
+        FG = fg;
+    }
+
+    public OutlineElement(Vector2i pos, int width, int height, RGB? bg = null, RGB? fg = null) {
+        this.X = pos.X;
+        this.Y = pos.Y;
+        Width = width;
+        Height = height;
+        BG = bg;
+        FG = fg;
+    }
+    
+    public override PixelMap Draw(PixelMap pixels) {
+        for (int y = 0; y < Height; y++) {
+            for (int x = 0; x < Width; x++) {
+                if (x == 0 && y == 0) {
+                    pixels.SetPixel(this, new Vector2i(x, y), new Pixel(tl, BG, FG));
+                } else if (x == Width - 1 && y == 0) {
+                    pixels.SetPixel(this, new Vector2i(x, y), new Pixel(tr, BG, FG));
+                } else if (x == 0 && y == Height - 1) {
+                    pixels.SetPixel(this, new Vector2i(x, y), new Pixel(bl, BG, FG));
+                } else if (x == Width - 1 && y == Height - 1) {
+                    pixels.SetPixel(this, new Vector2i(x, y), new Pixel(br, BG, FG));
+                } else if (x == 0 || x == Width - 1) {
+                    pixels.SetPixel(this, new Vector2i(x, y), new Pixel(v, BG, FG));
+                } else if (y == 0 || y == Height - 1) {
+                    pixels.SetPixel(this, new Vector2i(x, y), new Pixel(h, BG, FG));
+                }
+            }
+        }
+        return pixels;
+    }
+}
--- NSB.OS/Program.cs
+++ NSB.OS/Program.cs
@@ -9,11 +9,15 @@ namespace NSB.OS;
 
 public static class OS {
     public static void Main(String[] args) {
-        Display home = new Display(new Vector2i(0, 0), new Vector2i(80, 25));
-        Rectangle bg = new Rectangle(0, 0, 80, 25, new RGB(0, 0, 0), new RGB(0, 100, 255));
+        int width = 80;
+        int height = Console.WindowHeight;
+        Display home = new Display(new Vector2i(0, 0), new Vector2i(width, height));
+        Rectangle bg = new Rectangle(0, 0, width, height, new RGB(0, 0, 0), new RGB(0, 100, 255));
         home.AddElement(bg);
-        TextElement t = new TextElement(0, 0, "NSB_OS", null, null);
+        CenteredTextElement t = new CenteredTextElement(0, 1, "NSB_OS", width, null, null);
         home.AddElement(t);
+        OutlineElement o = new OutlineElement(0, 0, width, height, new RGB(0, 0, 0), new RGB(0, 100, 255));
+        home.AddElement(o);
 
         RendererStack renderer = new RendererStack();
         renderer.AddDisplay(home);
