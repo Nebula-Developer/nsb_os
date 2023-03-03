822e23e1a1eaf955a2b65cedce1ddffc4e303e48
NebulaDev <nebuladev.contact@gmail.com>
2023-03-03 17:31:58 +1300

    Add linked rendering program

---
+++ NSB.OS.Programs/Programs.LinkedProgram/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NSB.OS.Graphics.DisplayNS;
+using NSB.OS.Graphics.Mathematics;
+using NSB.OS.Graphics;
+
+namespace Programs.LinkedProgram;
+
+public static class Program {
+    public static void Run() {
+        Display display = new Display(new Vector2i(0, 0), new Vector2i(80, 25));
+        RectangleElement rectangle = new RectangleElement(0, 0, 80, 25, new RGB(0, 0, 0), new RGB(0, 0, 0));
+        CharElement charElement = new CharElement(0, 0, 'A', new RGB(0, 0, 0), new RGB(255, 255, 255));
+        display.AddElement(rectangle);
+        display.AddElement(charElement);
+
+        RendererStack renderer = new RendererStack();
+        renderer.AddDisplay(display);
+        renderer.Render(true);
+
+        Console.Out.Flush();
+        Console.ReadKey(true);
+    }
+}
